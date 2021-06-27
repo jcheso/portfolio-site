ce03f1b6a21c539015c36331030ec97c72489ed3
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-06-27 10:44:26 +0800

    Clean up Profile component code

--- web/src/components/Profile.js
+++ web/src/components/Profile.js
@@ -6,7 +6,7 @@ export default function Profile() {
       <main className="profile-page">
         <section className="relative block" style={{ height: "500px" }}>
           <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
+            className="absolute top-0 w-full h-full bg-center bg-cover bg-hero-pattern"
             style={{
               backgroundImage:
                 "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')",
@@ -55,37 +55,10 @@ export default function Profile() {
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                    <div className="py-6 px-3 mt-32 sm:mt-0">
-                      {/* <button
-                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
-                        type="button"
-                        style={{ transition: "all .15s ease" }}
-                      >
-                        Connect
-                      </button> */}
-                    </div>
+                    <div className="py-6 px-3 mt-32 sm:mt-0"></div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                    <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                      {/* <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          22
-                        </span>
-                        <span className="text-sm text-gray-500">Friends</span>
-                      </div>
-                      <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          10
-                        </span>
-                        <span className="text-sm text-gray-500">Photos</span>
-                      </div>
-                      <div className="lg:mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          89
-                        </span>
-                        <span className="text-sm text-gray-500">Comments</span>
-                      </div> */}
-                    </div>
+                    <div className="flex justify-center py-4 lg:pt-4 pt-8"></div>
                   </div>
                 </div>
                 <div className="text-center mt-12">
@@ -111,13 +84,6 @@ export default function Profile() {
                       <p className="mb-4 text-lg leading-relaxed text-gray-800">
                         About Me
                       </p>
-                      {/* <a
-                        href="#pablo"
-                        className="font-normal text-pink-500"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Show more
-                      </a> */}
                     </div>
                   </div>
                 </div>
