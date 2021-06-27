388f5b09cfb576499d4228b587ad138458e2e6a1
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-06-27 10:45:00 +0800

    Set index contents and remove unnecessary query

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,70 +1,26 @@
 import React from "react";
 import { graphql } from "gatsby";
-import {
-  filterOutDocsPublishedInTheFuture,
-  filterOutDocsWithoutSlugs,
-  mapEdgesToNodes,
-} from "../lib/helpers";
-import BlogPostPreviewList from "../components/blog-post-preview-list";
 import Container from "../components/container";
 import GraphQLErrorList from "../components/graphql-error-list";
 import SEO from "../components/seo";
 import Layout from "../containers/layout";
+import Header from "../components/Header";
+import Profile from "../components/Profile";
+import Footer from "../components/Footer";
+
+import "../styles/layout.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-export const query = graphql`
-  fragment SanityImage on SanityMainImage {
-    crop {
-      _key
-      _type
-      top
-      bottom
-      left
-      right
-    }
-    hotspot {
-      _key
-      _type
-      x
-      y
-      height
-      width
-    }
-    asset {
-      _id
-    }
-  }
-
-  query IndexPageQuery {
-    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
-      title
-      subtitle
-      description
-      keywords
-    }
-    posts: allSanityPost(
-      limit: 6
-      sort: { fields: [publishedAt], order: DESC }
-      filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
-    ) {
-      edges {
-        node {
-          id
-          publishedAt
-          mainImage {
-            ...SanityImage
-            alt
-          }
-          title
-          _rawExcerpt
-          slug {
-            current
-          }
-        }
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   query IndexPageQuery {
+//     site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+//       title
+//       subtitle
+//       description
+//       keywords
+//     }
+//   }
+// `;
 
 const IndexPage = (props) => {
   const { data, errors } = props;
@@ -77,37 +33,28 @@ const IndexPage = (props) => {
     );
   }
 
-  const site = (data || {}).site;
-  const postNodes = (data || {}).posts
-    ? mapEdgesToNodes(data.posts)
-        .filter(filterOutDocsWithoutSlugs)
-        .filter(filterOutDocsPublishedInTheFuture)
-    : [];
+  // const site = (data || {}).site;
 
-  if (!site) {
-    throw new Error(
-      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    );
-  }
+  // if (!site) {
+  //   throw new Error(
+  //     'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+  //   );
+  // }
 
   return (
-    <Layout>
-      <SEO
-        title={site.title}
-        description={site.description}
-        keywords={site.keywords}
-      />
-      <Container>
-        <p>{site.subtitle}</p>
-        {postNodes && (
-          <BlogPostPreviewList
-            title="Latest blog posts"
-            nodes={postNodes}
-            browseMoreHref="/archive/"
-          />
-        )}
-      </Container>
-    </Layout>
+    <>
+      <Header transparent="true" />
+      <Profile />
+      <Footer />
+    </>
+    // <Layout>
+    //   {/* <SEO
+    //     title={site.title}
+    //     description={site.description}
+    //     keywords={site.keywords}
+    //   /> */}
+
+    // </Layout>
   );
 };
 
