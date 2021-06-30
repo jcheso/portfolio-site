877f8fd1b6d3968c01b950dddf13d9380f5e1059
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-06-30 19:04:48 +0930

    wrap icons

--- web/src/components/skill.js
+++ web/src/components/skill.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skill = ({ logo, title }) => {
   return (
-    <div className="flex flex-col ml-8 mr-8 p-2">
+    <div className="flex flex-col mx-8 my-2 p-2">
       <div>
         <i className={logo}></i>
       </div>
--- web/src/containers/Skills.js
+++ web/src/containers/Skills.js
@@ -15,9 +15,9 @@ const SkillsContainer = () => {
         </div>
         <div className="mt-2 py-10 border-t border-gray-300 text-center">
           <div className="flex flex-wrap justify-center">
-            <div className="justify-center flex flex-1 mb-4 w-full lg:w-9/12 px-4 text-gray-800">
-              <Skill logo="fab fa-html5 fa-9x" title="HTML 5"></Skill>
-              <Skill logo="fab fa-css3 fa-9x" title="CSS 3"></Skill>
+            <div className="justify-center flex flex-1 mb-4 w-full lg:w-9/12 px-4 text-gray-800 flex-wrap">
+              <Skill logo="fab fa-html5 fa-9x" title="HTML5"></Skill>
+              <Skill logo="fab fa-css3 fa-9x" title="CSS3"></Skill>
               <Skill logo="fab fa-js fa-9x" title="JavaScript"></Skill>
               <Skill logo="fab fa-react fa-9x" title="React.js"></Skill>
               <Skill logo="fab fa-vuejs fa-9x" title="Vue.js"></Skill>
@@ -29,9 +29,11 @@ const SkillsContainer = () => {
         </div>
         <div className="mt-2 py-10 border-t border-gray-300 text-center">
           <div className="flex flex-wrap justify-center">
-            <div className="justify-center flex flex-1 mb-4 w-full lg:w-9/12 px-4 text-gray-800">
+            <div className="justify-center flex flex-1 mb-4 w-full lg:w-9/12 px-4 text-gray-800 flex-wrap">
               <Skill logo="fab fa-python fa-9x" title="Python"></Skill>
               <Skill logo="fab fa-node-js fa-9x" title="Node.js"></Skill>
+              <Skill logo="fas fa-database fa-9x" title="SQL"></Skill>
+              <Skill logo="far fa-copy fa-9x" title="NoSQL"></Skill>
             </div>
           </div>
         </div>
@@ -40,8 +42,9 @@ const SkillsContainer = () => {
         </div>
         <div className="mt-2 py-10 border-t border-gray-300 text-center">
           <div className="flex flex-wrap justify-center">
-            <div className="justify-center flex flex-1 mb-4 w-full lg:w-9/12 px-4 text-gray-800">
+            <div className="justify-center flex flex-1 mb-4 w-full lg:w-9/12 px-4 text-gray-800 flex-wrap">
               <Skill logo="fab fa-git fa-9x" title="Git"></Skill>
+              <Skill logo="fab fa-linux fa-9x" title="Linux"></Skill>
               <Skill logo="fab fa-aws fa-9x" title="AWS"></Skill>
             </div>
           </div>
