a8ecffda3f40a8d7979c48a0a3e2f7fd639fe796
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-07-06 09:29:37 +0930

    remove console logs

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -64,7 +64,7 @@ const IndexPage = (props) => {
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
     );
   }
-  console.log(site);
+
   return (
     <>
       <SEO
