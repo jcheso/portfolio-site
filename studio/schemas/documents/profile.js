0818dd2aa98e048e5ace06bbfc264eb65dbc019c
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-07-05 15:34:06 +0930

    added schema for profile

---
+++ studio/schemas/documents/profile.js
@@ -0,0 +1,50 @@
+export default {
+  name: "profile",
+  type: "document",
+  title: "Profile",
+  __experimental_actions: ["update", /*"create",  'delete', */ "publish"],
+
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Full Name",
+    },
+    {
+      name: "location",
+      type: "string",
+      title: "Location",
+    },
+    {
+      name: "job",
+      type: "string",
+      title: "Current Job",
+    },
+    {
+      name: "education",
+      type: "string",
+      title: "Education",
+    },
+    {
+      name: "aboutMe",
+      type: "text",
+      title: "About Me",
+    },
+    {
+      name: "skills",
+      title: "Skills",
+      type: "array",
+      of: [
+        {
+          name: "skill",
+          type: "reference",
+          to: [
+            {
+              type: "skill",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
