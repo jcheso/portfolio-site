e3f512679d7104efa1379d3d989576a407482a49
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-07-06 10:47:10 +0930

    switch styling to tailwind & add GatsbyImage

--- web/src/components/author-list.js
+++ web/src/components/author-list.js
@@ -1,34 +1,27 @@
-import * as styles from "./author-list.module.css";
 import React from "react";
-import { buildImageObj } from "../lib/helpers";
-import { imageUrlFor } from "../lib/image-url";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 function AuthorList({ items, title }) {
   return (
-    <div className={styles.root}>
+    <div className="mt-6 mb-3">
       <h2 className="text-l pt-4 pb-2 font-bold leading-normal text-gray-800">
         {title}
       </h2>
-      <ul className={styles.list}>
+      <ul className="m-0 p-0 list-none">
         {items.map(({ author, _key }) => {
           const authorName = author && author.name;
           return (
-            <li key={_key} className={styles.listItem}>
-              <div>
-                <div className={styles.avatar}>
-                  {author && author.image && author.image.asset && (
-                    <img
-                      src={imageUrlFor(buildImageObj(author.image))
-                        .width(100)
-                        .height(100)
-                        .fit("crop")
-                        .url()}
-                      alt=""
-                    />
-                  )}
-                </div>
+            <li key={_key} className="mt-1 flex justify-left items-center ">
+              <div className="ml-0.5 mr-2">
+                {author && author.image && author.image.asset && (
+                  <GatsbyImage
+                    className="relative h-12 w-12 rounded-full overflow-hidden"
+                    image={author.image.asset.gatsbyImageData}
+                    alt="Author Portrait"
+                  />
+                )}
               </div>
-              <div>
+              <div className="text-sm leading-normal my-4 text-gray-800 ">
                 <div>{authorName || <em>Missing name</em>}</div>
               </div>
             </li>
