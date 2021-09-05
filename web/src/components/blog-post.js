454515d2bc408788ac9b9328a827f446f0cd4888
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-09-05 11:57:47 +0930

    update blog formatting

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -1,14 +1,18 @@
 import { differenceInDays, formatDistance, format } from "date-fns";
 import AuthorList from "./author-list";
 import Container from "./container";
-import PortableText from "./portableText";
+// import PortableText from "./portableText";
 import React from "react";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import PortableText from "react-portable-text";
+import { Figure } from "./Figure";
+import BlockContent from "@sanity/block-content-to-react";
 
 function BlogPost(props) {
   const { _rawBody, authors, categories, title, mainImage, publishedAt } =
     props;
   const image = getImage(mainImage.asset.gatsbyImageData);
+  console.log(_rawBody);
   return (
     <article>
       {mainImage && mainImage.asset && (
@@ -22,7 +26,23 @@ function BlogPost(props) {
             <h1 className="text-2xl font-bold leading-normal mb-8 text-gray-800">
               {title}
             </h1>
-            {_rawBody && <PortableText blocks={_rawBody} />}
+            {/* {_rawBody && <PortableText blocks={_rawBody} />} */}
+            <PortableText
+              // Pass in block content straight from Sanity.io
+              content={_rawBody}
+              // Optionally override marks, decorators, blocks, etc. in a flat
+              // structure without doing any gymnastics
+              serializers={{
+                h1: (props) => <h1 style={{ color: "red" }} {...props} />,
+                li: ({ children }) => (
+                  <li className="special-list-item">{children}</li>
+                ),
+                normal: (props) => (
+                  <p className="my-4 text-justify" {...props} />
+                ),
+                mainImage: Figure,
+              }}
+            />
           </div>
           <aside className="divide-y divide-light-gray-400">
             {publishedAt && (
