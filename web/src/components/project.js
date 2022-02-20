38686b1881f10517f9b6b578b8f262c007c7d4e4
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-02-20 10:12:12 +0000

    improve responsiveness

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -6,21 +6,21 @@ var uniqid = require("uniqid");
 
 const ProjectCMS = (props) => {
   return (
-    <div className="p-4 md:w-1/3 lg:w-1/5">
+    <div className="p-4 md:w-1/3 xl:w-3/8">
       <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <GatsbyImage
           className="lg:h-96 md:h-36 w-full object-cover object-center"
           image={props.img}
           alt="blog"
         />
-        <div className="p-6">
+        <div className="p-6 flex flex-col align-middle">
           {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
             CATEGORY
           </h2> */}
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
             {props.title}
           </h1>
-          <p className="leading-relaxed mb-3 md:h-28">{props.description}</p>
+          <p className="leading-relaxed mb-3 xl:h-48">{props.description}</p>
           <div className="flex items-center flex-wrap pt-2 ">
             <OutboundLink
               href={props.link}
